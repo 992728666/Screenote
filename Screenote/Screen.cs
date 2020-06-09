880159c547f54a28f8ce85a4c7eb5da6878ceaa8
Poerin <Poerin@live.com>
2020-06-09 17:15:28 +0800

    2020-06-09

--- Screenote/Screen.cs
+++ Screenote/Screen.cs
@@ -235,43 +235,41 @@ namespace Screenote
 
         private void picture_MouseMove(object sender, MouseEventArgs e)
         {
-            if (DateTime.Now.Ticks - tick < 400000)
+            if ((DateTime.Now.Ticks - tick > 400000) || (e.X == this.Left && e.Y == this.Top) || (e.X == this.Right - 1 && e.Y == this.Top) || (e.X == this.Left && e.Y == this.Bottom - 1) || (e.X == this.Right - 1 && e.Y == this.Bottom - 1))
             {
-                return;
-            }
-            tick = DateTime.Now.Ticks;
+                tick = DateTime.Now.Ticks;
 
-            this.Refresh();
-            int X = Cursor.Position.X, Y = Cursor.Position.Y;
-            magnifier.Location = new Point(X + 150 > this.Width ? X - 150 : X + 50, Y + 150 > this.Height ? Y - 150 : Y + 50);
+                this.Refresh();
+                int X = Cursor.Position.X, Y = Cursor.Position.Y;
+                magnifier.Location = new Point(X + 150 > this.Width ? X - 150 : X + 50, Y + 150 > this.Height ? Y - 150 : Y + 50);
 
-            int left = X - 12 < 0 ? 12 - X : 0;
-            int right = X + 13 > this.Width ? (X + 13 - this.Width) : 0;
-            int top = Y - 12 < 0 ? 12 - Y : 0;
-            int bottom = Y + 13 > this.Height ? (Y + 13 - this.Height) : 0;
+                int left = X - 12 < 0 ? 12 - X : 0;
+                int right = X + 13 > this.Width ? (X + 13 - this.Width) : 0;
+                int top = Y - 12 < 0 ? 12 - Y : 0;
+                int bottom = Y + 13 > this.Height ? (Y + 13 - this.Height) : 0;
 
-            Rectangle region = new Rectangle(X - 12 + left, Y - 12 + top, 25 - right, 25 - bottom);
-            bitmapTarget = bitmapScreen.Clone(region, System.Drawing.Imaging.PixelFormat.Format24bppRgb);
-            Bitmap bitmapMagnifier = new Bitmap(100, 100);
-            graphicsMagnifier = Graphics.FromImage(bitmapMagnifier);
+                Rectangle region = new Rectangle(X - 12 + left, Y - 12 + top, 25 - right, 25 - bottom);
+                bitmapTarget = bitmapScreen.Clone(region, System.Drawing.Imaging.PixelFormat.Format24bppRgb);
+                Bitmap bitmapMagnifier = new Bitmap(100, 100);
+                graphicsMagnifier = Graphics.FromImage(bitmapMagnifier);
 
-            for (int y = 0; y < region.Height; y++)
-            {
-                for (int x = 0; x < region.Width; x++)
+                for (int y = 0; y < region.Height; y++)
                 {
-                    graphicsMagnifier.FillRectangle(new SolidBrush(bitmapTarget.GetPixel(x, y)), (x + left) * 4, (y + top) * 4, 4, 4);
+                    for (int x = 0; x < region.Width; x++)
+                    {
+                        graphicsMagnifier.FillRectangle(new SolidBrush(bitmapTarget.GetPixel(x, y)), (x + left) * 4, (y + top) * 4, 4, 4);
+                    }
+                }
+
+                bitmapTarget.Dispose();
+                graphicsMagnifier.FillRectangles(new SolidBrush(Color.FromArgb(192, 192, 192)), new Rectangle[] { new Rectangle(48, 0, 4, 48), new Rectangle(48, 52, 4, 48), new Rectangle(0, 48, 48, 4), new Rectangle(52, 48, 48, 4) });
+                magnifier.Image = bitmapMagnifier;
+                graphicsScreen.FillRectangles(new SolidBrush(Color.FromArgb(192, 192, 192)), new Rectangle[] { new Rectangle(X, 0, 1, this.Height), new Rectangle(0, Y, this.Width, 1) });
+                if (shot)
+                {
+                    graphicsScreen.FillRectangles(new SolidBrush(Color.FromArgb(192, 192, 192)), new Rectangle[] { new Rectangle(Start.X, 0, 1, this.Height), new Rectangle(0, Start.Y, this.Width, 1) });
                 }
             }
-
-            bitmapTarget.Dispose();
-            graphicsMagnifier.FillRectangles(new SolidBrush(Color.FromArgb(192, 192, 192)), new Rectangle[] { new Rectangle(48, 0, 4, 48), new Rectangle(48, 52, 4, 48), new Rectangle(0, 48, 48, 4), new Rectangle(52, 48, 48, 4) });
-            magnifier.Image = bitmapMagnifier;
-            graphicsScreen.FillRectangles(new SolidBrush(Color.FromArgb(192, 192, 192)), new Rectangle[] { new Rectangle(X, 0, 1, this.Height), new Rectangle(0, Y, this.Width, 1) });
-            if (shot)
-            {
-                graphicsScreen.FillRectangles(new SolidBrush(Color.FromArgb(192, 192, 192)), new Rectangle[] { new Rectangle(Start.X, 0, 1, this.Height), new Rectangle(0, Start.Y, this.Width, 1) });
-            }
-
         }
 
         private void Screen_KeyDown(object sender, KeyEventArgs e)
